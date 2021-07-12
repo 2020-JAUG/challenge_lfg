446599dc3f183e6afd75b932f334f9ab62c7e06d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-12 21:58:14 +0200

    ER Created

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    //ENCAPSULAMOS LOS ATRIBUTOS
+    protected $fillable = ['title', 'thumbnail_url', 'user_id'];
+
+    public function party ()
+    {
+        return $this ->hasMany(Party::class);
+    }
 }
