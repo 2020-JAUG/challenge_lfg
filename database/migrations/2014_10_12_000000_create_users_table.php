0ded458fdc1bae5728a7148d72645ebb4e916ff3
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-14 23:05:55 +0200

    Change the row game_id on table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('game_id')->unsigned();
+            $table->foreignId('game_id')->references('id')->on('users');
             $table->string('steamUsername', 20)->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
